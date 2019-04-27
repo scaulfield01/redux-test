18b5834ee84c136aafc215a2b3c5c0f152414be2
scaulfield01 <s.caulfield88@gmail.com>
2019-04-26 21:48:11 -0700

    add redux think to create fakey API

--- src/index.js
+++ src/index.js
@@ -5,20 +5,11 @@ import App from './App';
 import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
-import {bindActionCreators} from 'redux';
-import {updateCurrent} from './reducers/todo';
-
-// const todoChangeHandler = (val) => 
-// 	store.dispatch(updateCurrent(val))
-
-const actions = bindActionCreators({updateCurrent}, store.dispatch)
-
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App changeCurrent={actions.updateCurrent}/>
+		<App />
   </Provider>,
 document.getElementById('root'));
-
 
 serviceWorker.unregister();
